73ecfa1b6b6f6e21eee009cd0e256cc95a962949
Ruslan <sabbath9993@gmail.com>
2020-10-30 19:39:48 +0300

    first commit

--- app/js/burger-menu.js
+++ app/js/burger-menu.js
@@ -1,5 +1,6 @@
 $('.menu-toggle').on('click', function(){
   $('body').toggleClass('open hide');
+  // $('.mobile__menu').css('display', 'block')
   // $('body').css('overflow', 'hidden');
   // $('body').toggleClass('hide');
 });
